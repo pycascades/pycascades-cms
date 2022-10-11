57678f2ab3a676336a4325aaec4f576550c29612
Madison Swain-Bowden <bowdenm@spu.edu>
2022-10-10 22:30:54 -0700

    Change setting name

--- pycascades/settings/base.py
+++ pycascades/settings/base.py
@@ -170,7 +170,8 @@ WAGTAIL_SITE_NAME = "pycascades"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "http://example.com"
+WAGTAILADMIN_BASE_URL = "http://example.com"
+
 
 BAKERY_VIEWS = (
     "wagtailbakery.views.AllPublishedPagesView",
