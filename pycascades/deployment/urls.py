12d78723bc72b278a3fa3d1eca2d267a4cced1d9
Madison Swain-Bowden <bowdenm@spu.edu>
2022-10-14 19:08:09 -0700

    Fix paths

--- pycascades/deployment/urls.py
+++ pycascades/deployment/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import redirects, success_hook
 
 
 urlpatterns = [
-    url(r"^success$", success_hook, name="success_hook"),
-    url(r"^redirects$", redirects, name="redirect_builder"),
+    re_path(r"^success$", success_hook, name="success_hook"),
+    re_path(r"^redirects$", redirects, name="redirect_builder"),
 ]
