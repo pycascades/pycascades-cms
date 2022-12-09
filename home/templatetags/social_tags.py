b4dfa1d8841ec6f6f27e62da77aee6e1c823714d
Madison Swain-Bowden <bowdenm@spu.edu>
2022-12-09 15:53:53 -0800

    Pull remove_scheme_prefix into this branch, use for verification url

--- home/templatetags/social_tags.py
+++ home/templatetags/social_tags.py
@@ -9,3 +9,8 @@ register = template.Library()
 @register.inclusion_tag("tags/mastodon.html")
 def mastodon_verification():
     return {"accounts": MastodonVerification.objects.all()}
+
+
+@register.filter
+def remove_scheme_prefix(url):
+    return url.replace("https://", "").replace("http://", "")
